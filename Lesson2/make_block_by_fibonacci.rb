b4e172bf81ec01e0ba60cfc45ec260c91c7c59a1
alexleskov <dsl_92@mail.ru>
2018-06-28 21:15:10 +0300

    Lesson 2: Tasks 1-5. Some fix and added new task

--- Lesson2/make_block_by_fibonacci.rb
+++ Lesson2/make_block_by_fibonacci.rb
@@ -1,9 +1,13 @@
 #Заполнить массив числами фибоначчи до 100
 
 massif = [0,1]
+n = 2
 
-for n in 2..100
-  massif.push(massif[n-1] + massif[n-2])
+until massif.last >= 100 do
+  massif << (massif[n-1] + massif[n-2])
+  n += 1
 end
 
-puts "massif: #{massif}"
+massif.delete_if {|number| number > 100 }
+
+puts "Massif: #{massif}"
--- Lesson2/make_block_by_num.rb
+++ Lesson2/make_block_by_num.rb
@@ -6,4 +6,4 @@ for i in 10..100
   massif.push(i) if i % 5 == 0
 end
 
-puts "massif: #{massif}"
+puts "Massif: #{massif}"
