2eb805b75c8d5c03140973a10e5c731c735f605a
alexleskov <dsl_92@mail.ru>
2018-07-01 12:35:26 +0300

    Lesson 2: Tasks 1-6 (Edition #3)

--- Lesson2/make_hash_by_vowels.rb
+++ Lesson2/make_hash_by_vowels.rb
@@ -4,8 +4,8 @@ alphabet = ('a'..'z')
 vowels = ['a','e','i','o','u']
 hash = {}
 
-alphabet.each.with_index(1) { |letter, index|
+alphabet.each.with_index(1) do |letter, index|
   hash[letter] = index if vowels.include? letter
-}
+end
 
 puts "Hash: #{hash}"
