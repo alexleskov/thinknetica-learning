0b85487eda347ed21c6b197bb8e98a3d891c54f5
alexleskov <39882467+alexleskov@users.noreply.github.com>
2018-07-11 16:01:06 +0300

    Lesson 3: Station, Route, Train (Edition 4.2)
    Some fixes for route.rb

--- Lesson3/route.rb
+++ Lesson3/route.rb
@@ -1,5 +1,5 @@
 class Route
-  attr_reader :stations, :station_first, :station_last
+  attr_reader :stations
 
   def initialize(departure, arrival)
     if departure.class == Station && arrival.class == Station
@@ -13,17 +13,16 @@ class Route
     end
   end
 
-  def station_first
+  def first_station
     stations.first
   end
 
-  def station_last
+  def last_station
     stations.last
   end
 
   def station_remove(station)
-    if station.class == Station && stations.include?(station) &&
-      station != station_first && station != station_last
+    if [first_station, last_station].index(station).nil?
       @stations.delete(station)
     end
   end
@@ -32,13 +31,6 @@ class Route
     print "Список всех станций по порядку: "
     stations.each do |station|
       print "-[#{station.name}]-"
-    end
-  end
-  
-  def stations_between_list
-    print "Промежуточные станции: "
-    stations.each do |station|
-      print "-[#{station.name}]-" if station != station_first && station != station_last
     end
   end
 end
