90431fdee8a755b8d7016f932f78feeafb63553a
alexleskov <39882467+alexleskov@users.noreply.github.com>
2018-07-12 17:38:42 +0300

    Lesson 3: Station, Route, Train (Edition 6)

--- Lesson3/station.rb
+++ Lesson3/station.rb
@@ -8,26 +8,14 @@ class Station
 
   def train_get(train)
     return if trains.include?(train) || train.class != Train
-      @trains << train
+    @trains << train
   end
 
   def train_send(train)
-    if trains.include?(train)
-      @trains.delete(train)
-    end
-  end
-
-  def trains_type
-    trains.type
+    @trains.delete(train) if trains.include?(train)
   end
 
   def trains_count_by_type(type)
-    trains_types = []
-      trains.each { |train| trains_types << train.type }
-    if type == :passenger
-      passenger_trains = trains_types.count(type)
-    elsif type == :cargo
-      cargo_trains_count = trains_types.count(type)
-    end
+    @trains.count { |train| train.types == type }
   end
 end
