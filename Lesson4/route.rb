abf58555a143b42e4a872ae82539523e13dbf10b
alexleskov <dsl_92@mail.ru>
2018-08-13 22:11:24 +0300

    Lesson 4 Reload

---
+++ Lesson4/route.rb
@@ -0,0 +1,36 @@
+class Route
+  attr_reader :stations, :name
+
+  def initialize(name, departure, arrival)
+    if departure.instance_of?(Station) && arrival.instance_of?(Station)
+      @name = name
+      @stations = [departure, arrival]
+    end
+  end
+
+  def station_add(station)
+    return if stations.include?(station) || !station.instance_of?(Station)
+    @stations.insert(-2, station)
+  end
+
+  def station_remove(station)
+    return if [first_station, last_station].include?(station)
+    @stations.delete(station)
+  end
+
+  def stations_list
+    print "Список всех станций по порядку: "
+    @stations.each do |station|
+      print "-[#{station.name}]-"
+    end
+  end
+
+  def first_station
+    @stations.first
+  end
+
+  def last_station
+    @stations.last
+  end
+  
+end 
---
+++ Lesson4/wagon_cargo.rb
@@ -0,0 +1,8 @@
+class CargoWagon < Wagon
+
+  def initialize(number)
+    @type = :cargo
+    super
+  end 
+  
+end 
